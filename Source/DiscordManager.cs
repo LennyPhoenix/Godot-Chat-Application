babf1e6a9892736d1f88119ef015ac1b73fdf816
Lenny <LennyPhoenixC@gmail.com>
2020-10-07 18:54:25 +0100

    Refactored activity code.

--- Source/DiscordManager.cs
+++ Source/DiscordManager.cs
@@ -4,5 +4,55 @@ using Discord;
 
 public class DiscordManager : Node
 {
-    public Discord.Discord discord = new Discord.Discord(763426273757495327, (ulong)Discord.CreateFlags.Default);
+    public Discord.Discord discord;
+    public ActivityManager activityManager;
+
+    long startTime;
+
+    public override void _Ready()
+    {
+        discord = new Discord.Discord(763426273757495327, (ulong)Discord.CreateFlags.Default);
+        activityManager = discord.GetActivityManager();
+
+        UpdateActivity(true);
+    }
+
+    public void UpdateActivity(bool resetTime = false)
+    {
+        if (resetTime)
+        {
+            startTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        }
+
+        Activity activity = new Activity
+        {
+            State = "In room.",
+            Timestamps =
+            {
+                Start=startTime
+            },
+            Assets =
+            {
+                LargeImage="logo",
+                LargeText="Godot Chat App"
+            }
+        };
+
+        activityManager.UpdateActivity(activity, (result) =>
+        {
+            if (result == Result.Ok)
+            {
+                GD.Print("Successfully updated activity.");
+            }
+            else
+            {
+                GD.PrintErr("Failed to update activity: ", result);
+            }
+        });
+    }
+
+    public override void _Process(float delta)
+    {
+        discord.RunCallbacks();
+    }
 }
