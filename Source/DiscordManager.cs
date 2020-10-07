ea4226a0a5d81b9a8a71c3310d2a8071f7ee2895
Lenny <LennyPhoenixC@gmail.com>
2020-10-07 17:21:54 +0100

    Updated node types.

--- Source/DiscordManager.cs
+++ Source/DiscordManager.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 using Discord;
 
-public class DiscordManager : Node2D
+public class DiscordManager : Node
 {
     public Discord.Discord discord = new Discord.Discord(763426273757495327, (ulong)Discord.CreateFlags.Default);
 }
